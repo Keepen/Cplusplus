003b65afea571d9057df0cca787f75364193a1de
Keepen <46278766+Keepen@users.noreply.github.com>
2020-01-20 01:59:38 +0800

    二叉树的深度

---
+++ 二叉树的深度/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+		val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+	int TreeDepth(TreeNode* pRoot)
+	{
+		if (pRoot == NULL) {
+			return 0;
+		}
+		int left = TreeDepth(pRoot->left);
+		int right = TreeDepth(pRoot->right);
+		return (left > right ? left : right) + 1;
+	}
+};
+
+int main() {
+	system("pause");
+	return 0;
+}
